ff4b2cec877b804fc33d1dcd6c29565526421607
Iyasere Elizabeth <iyasere.elizabeth1@gmail.com>
2025-01-07 10:14:51 +0100

    Real Estate App-My First React Native Project

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,18 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold text-lg my-10 text-green">
+        Welcome to Restate
+      </Text>
+    </View>
+  );
+}
